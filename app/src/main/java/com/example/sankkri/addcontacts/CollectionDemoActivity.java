3df5cc8fda738ace311b682c128e6614be98248e
krishnaths <rocky_krish@live.com>
2016-07-31 00:18:48 +0200

    Fixed Compatibility issues

--- app/src/main/java/com/example/sankkri/addcontacts/CollectionDemoActivity.java
+++ app/src/main/java/com/example/sankkri/addcontacts/CollectionDemoActivity.java
@@ -21,7 +21,7 @@ public class CollectionDemoActivity extends FragmentActivity {
     DemoCollectionPagerAdapter mDemoCollectionPagerAdapter;
     ViewPager mViewPager;
 
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_srch);
--- app/src/main/java/com/example/sankkri/addcontacts/srchActivity.java
+++ app/src/main/java/com/example/sankkri/addcontacts/srchActivity.java
@@ -3,49 +3,68 @@ package com.example.sankkri.addcontacts;
 import android.app.ActionBar;
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.TextView;
 
-public class srchActivity extends FragmentActivity implements ActionBar.TabListener{
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class srchActivity extends FragmentActivity implements ActionBar.TabListener {
 
 
     AppSectionsPagerAdapter mAppSectionsPagerAdapter;
     ViewPager mViewPager;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+
         setContentView(R.layout.activity_srch);
 
         mAppSectionsPagerAdapter = new AppSectionsPagerAdapter(getSupportFragmentManager());
 
 
         final ActionBar actionBar = getActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
+        System.out.println(actionBar);
+        actionBar.setHomeButtonEnabled(true);
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
         mViewPager = (ViewPager) findViewById(R.id.pager);
         mViewPager.setAdapter(mAppSectionsPagerAdapter);
-        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener(){
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
             @Override
-            public void onPageSelected(int position){
+            public void onPageSelected(int position) {
                 actionBar.setSelectedNavigationItem(position);
             }
         });
 
-        for(int i =0; i < mAppSectionsPagerAdapter.getCount(); i++){
+        for (int i = 0; i < mAppSectionsPagerAdapter.getCount(); i++) {
             actionBar.addTab(actionBar.newTab().setText(mAppSectionsPagerAdapter.getPageTitle(i)).setTabListener(this));
         }
 
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -63,72 +82,111 @@ public class srchActivity extends FragmentActivity implements ActionBar.TabListe
 
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
 
-    public static class AppSectionsPagerAdapter extends FragmentPagerAdapter{
-        public AppSectionsPagerAdapter(FragmentManager fm){
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "srch Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.sankkri.addcontacts/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "srch Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.sankkri.addcontacts/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
+
+    public static class AppSectionsPagerAdapter extends FragmentPagerAdapter {
+        public AppSectionsPagerAdapter(FragmentManager fm) {
             super(fm);
         }
 
         @Override
-        public Fragment getItem(int i){
-            switch(i){
+        public Fragment getItem(int i) {
+            switch (i) {
                 case 0:
                     return new LaunchpadSectionFragment();
                 default:
                     Fragment fragment = new DummySectionFragment();
                     Bundle args = new Bundle();
-                            args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, i+1);
+                    args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, i + 1);
                     fragment.setArguments(args);
                     return fragment;
             }
         }
 
         @Override
-        public int getCount()
-        {
+        public int getCount() {
             return 3;
         }
 
         @Override
-        public CharSequence getPageTitle(int position){
+        public CharSequence getPageTitle(int position) {
             return "Section " + (position + 1);
         }
     }
 
-    public static class LaunchpadSectionFragment extends Fragment{
+    public static class LaunchpadSectionFragment extends Fragment {
 
-         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInsBundle){
-             View rootView = inflater.inflate(R.layout.fragment_section_launchpad, container, false);
-             rootView.findViewById(R.id.demo_collection_button).setOnClickListener(new View.OnClickListener(){
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInsBundle) {
+            View rootView = inflater.inflate(R.layout.fragment_section_launchpad, container, false);
+            rootView.findViewById(R.id.demo_collection_button).setOnClickListener(new View.OnClickListener() {
 
-                 @Override
-                 public void onClick(View view){
-                     Intent intent = new Intent(getActivity(), CollectionDemoActivity.class);
-                     startActivity(intent);
-                 }
-             });
-             rootView.findViewById(R.id.demo_external_activity).setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(getActivity(), CollectionDemoActivity.class);
+                    startActivity(intent);
+                }
+            });
+            rootView.findViewById(R.id.demo_external_activity).setOnClickListener(new View.OnClickListener() {
 
-                 @Override
-                 public void onClick(View view){
-                     Intent externalActivityIntent = new Intent(Intent.ACTION_PICK);
-                     externalActivityIntent.setType("image/*");
-                     externalActivityIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
-                     startActivity(externalActivityIntent);
-                 }
-             });
+                @Override
+                public void onClick(View view) {
+                    Intent externalActivityIntent = new Intent(Intent.ACTION_PICK);
+                    externalActivityIntent.setType("image/*");
+                    externalActivityIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+                    startActivity(externalActivityIntent);
+                }
+            });
 
-             return rootView;
-         }
+            return rootView;
+        }
     }
 
 
-    public static class DummySectionFragment extends Fragment{
+    public static class DummySectionFragment extends Fragment {
         public static final String ARG_SECTION_NUMBER = "section_number";
 
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_section_dummy, container, false);
             Bundle args = getArguments();
             ((TextView) rootView.findViewById(android.R.id.text1)).setText(getString(R.string.dummy_section_text, args.getInt(ARG_SECTION_NUMBER)));
